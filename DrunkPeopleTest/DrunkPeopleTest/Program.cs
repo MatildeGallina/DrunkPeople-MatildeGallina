a56353aee97c836b30a965bfab41340991d2610f
matilde gallina <matilde.gallina@outlook.com>
2018-07-19 10:12:42 +0200

    class Pub

---
+++ DrunkPeopleTest/DrunkPeopleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrunkPeopleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
